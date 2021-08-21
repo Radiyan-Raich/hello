a8eff9ec026831e914fc7b736d1e72610f0e94da
RadiyanRaich <radiyan.raich@07.com>
2021-08-21 06:02:24 -0700

    first

---
+++ assing.js
@@ -0,0 +1,15 @@
+document.getElementById('8GB-button').addEventListener('click', function(){
+    console.log(9)
+})
+
+// document.getElementById('submit').addEventListener
+// ('click', function promocode(){
+// const PromoField = document.getElementById('promo-code');
+// const  PromoCode = PromoField.innerText;
+
+// if(PromoCode == 'stevekaku'){
+//     const totalprice = document.getElementById('Total-price');
+//     const UsePromo = totalprice / 20;
+
+// }
+// })
